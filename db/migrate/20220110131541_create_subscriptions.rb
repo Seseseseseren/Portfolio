f61b37b7e2811592e9d645b2cda0921426f2c413
EC2 Default User <itoseren@gmail.com>
2022-01-13 10:12:22 +0000

    [fix]maigrate

--- db/migrate/20220110131541_create_subscriptions.rb
+++ db/migrate/20220110131541_create_subscriptions.rb
@@ -1,11 +1,11 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-      
+
       t.integer :genre_id
       t.string :subscription_name,null: false, default: ""
       t.string :subscription_image_id
-      t.integer :subscription_fee,null: false, default: ""
+      t.integer :subscription_fee,null: false, default: 0
       t.string :payment_date,null: false, default: ""
 
       t.timestamps
