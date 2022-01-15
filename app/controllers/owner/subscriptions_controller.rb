85d64c2e19b51d2b5e1c3fdb6a711ccf7795c3fb
EC2 Default User <itoseren@gmail.com>
2022-01-15 14:31:52 +0000

    [add]owner.subscriptionモデル、コントローラー

--- app/controllers/owner/subscriptions_controller.rb
+++ app/controllers/owner/subscriptions_controller.rb
@@ -1,2 +1,44 @@
 class Owner::SubscriptionsController < ApplicationController
+  before_action :authenticate_owner!
+  
+  def index
+    @subscriptions = Subscription.all
+  end
+  
+  def new
+    @subscription = Subscription.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @subscription = Subscription.new(subscription_params)
+    if @subscription.save
+      redirect_to owner_subscription_path(@subscription)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @subscription = Subscription.find(params[:id])
+  end
+  
+  def edit
+    @subscription = Subscription.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    @subscription = Subscription.find(params[:id])
+    if @subscription.update(subscription_params)
+      redirect_to owner_subscription_path(@subscription)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  def subscription_params
+    params.require(:subscription).permit(:name,)
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,10 @@
 class Subscription < ApplicationRecord
+  
+  belongs_to :genre
+  has_many :reviews, dependent: :destroy
+  has_many :subscription_members
+  
+  attachment :image
+
+  
 end
