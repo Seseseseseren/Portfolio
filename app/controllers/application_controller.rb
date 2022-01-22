926d95c063be9d83a07f5b65ab932aa33e41e727
EC2 Default User <itoseren@gmail.com>
2022-01-22 03:51:08 +0000

    [fix]TOPページレイアウト修正、ロゴ画像格納

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
     def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:family_name,:first_name,:family_name_kana,:first_name_kana])
     end
-    
+
     def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Owner)
-      owner_root_path
+      owner_subscriptions_path
     else
     root_path
     end
