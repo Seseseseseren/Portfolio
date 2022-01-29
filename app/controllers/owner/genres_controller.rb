710103d0853b0d847f48558f8c4b2f02cb6dfc4a
EC2 Default User <itoseren@gmail.com>
2022-01-29 06:24:56 +0000

    [fix]オーナーページレイアウト修正・バリデーション設定

--- app/controllers/owner/genres_controller.rb
+++ app/controllers/owner/genres_controller.rb
@@ -12,15 +12,22 @@ class Owner::GenresController < ApplicationController
 
   def create
     genre = Genre.new(genre_params)
-    genre.save
-    redirect_back(fallback_location: root_path)
+    if genre.save
+      flash[:success] = "ジャンルの登録が完了しました"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:danger] = "ジャンル登録に失敗しました"
+      redirect_to request.referer
+    end
   end
 
   def update
     genre = Genre.find(params[:id])
     if genre.update(genre_params)
+      flash[:success] = "更新が完了しました"
       redirect_to owner_genres_path
     else
+      flash[:danger] = "更新に失敗しました"
       redirect_to request.referer
     end
   end
