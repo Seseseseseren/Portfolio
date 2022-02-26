2f67aa6c9f9b6d914bcb6b2d8a13805cc610849d
EC2 Default User <itoseren@gmail.com>
2022-02-26 02:38:17 +0000

    [fix]rateがの最小値のバリデーション変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -12,7 +12,7 @@ class Review < ApplicationRecord
 
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1,
+    greater_than_or_equal_to: 0.5,
   }, presence: true
 
 end
